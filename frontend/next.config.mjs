50a7263ec35e9b5e80c8a6dca37b3923e95262b0
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-07-22 23:20:09 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,   // This disables sharp completely
+  },
   reactStrictMode: true,
+  // Force serverless for Vercel
+  output: 'standalone',
 };
 
 export default nextConfig;
